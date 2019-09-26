b502b3b44b7f4850e6230be7a09999ee5ec8860b
Jim Geurts <jim@biacreations.com>
2019-09-25 21:29:42 -0500

    Replace tslint with eslint

---
+++ tests/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Override our default settings just for this directory
+  env: {
+    mocha: true,
+    es6: true
+  },
+  globals: {
+    describe: true,
+    before: true,
+    after: true,
+    beforeEach: true,
+    afterEach: true,
+    it: true
+  },
+}
