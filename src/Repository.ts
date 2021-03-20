09ac0e3d154a6de6e1a12fea19881076b6118e14
Jim Geurts <jim@biacreations.com>
2021-03-20 09:06:33 -0500

    Explicitly specify beforeCreate arguments

--- src/Repository.ts
+++ src/Repository.ts
@@ -62,7 +62,7 @@ export class Repository<T extends Entity> extends ReadonlyRepository<T> implemen
     if (this._type.beforeCreate) {
       if (Array.isArray(values)) {
         // eslint-disable-next-line no-param-reassign
-        values = await Promise.all(values.map(this._type.beforeCreate));
+        values = await Promise.all(values.map((value) => (this._type.beforeCreate ? this._type.beforeCreate(value) : Promise.resolve())));
       } else {
         // eslint-disable-next-line no-param-reassign
         values = await this._type.beforeCreate(values);
