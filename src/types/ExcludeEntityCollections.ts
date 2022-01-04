c61b1da28989db72d9c75c36079dc5172a20da4a
Jim Geurts <jim@biacreations.com>
2022-01-03 18:22:49 -0600

    Formatting updates

--- src/types/ExcludeEntityCollections.ts
+++ src/types/ExcludeEntityCollections.ts
@@ -4,7 +4,7 @@ import type { Entity, NotEntityBrand } from '../Entity';
  * Removes all entity collection properties. To be used as a re-map key function
  */
 export type ExcludeEntityCollections<T, K extends PropertyKey> = T extends NotEntityBrand[] | undefined
-  ? K // Return the key if collection is NotEntityBrand
+  ? K // Return the key if collection is a NotEntityBrand array
   : T extends Entity[] | undefined
   ? never // If T is an entity array, remove
   : K; // Otherwise, return the key
