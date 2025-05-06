505e7c98dee57317b037c36dc7d3ffedb8b8052d
Jim Geurts <jim@biacreations.com>
2025-05-06 09:42:27 -0500

    Disable unused testing libraries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import { config } from 'eslint-config-decent';
 export default [
   ...config({
     tsconfigRootDir: import.meta.dirname,
+    enableJest: false,
+    enableVitest: false,
+    enableTestingLibrary: false,
   }),
   {
     files: ['**/*.ts'],
